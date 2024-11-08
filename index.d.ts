d07de641ed33f206f267b5dcb143ad4c76ed8e08
csandman <chris@getcuebox.com>
2024-11-07 20:07:35 -0500

    Update eslint config and packages

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function wordsToNumbers(
-  text: string,
-  options?: { fuzzy: boolean }
-): string | number | null;
-export default wordsToNumbers;
---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  target: "es2020",
+  dts: true,
+  treeshake: true,
+});
